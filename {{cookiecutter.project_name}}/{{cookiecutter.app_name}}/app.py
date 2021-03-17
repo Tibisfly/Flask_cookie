fbce78da45ca3df976f14f8e9e7d086d76fcfc60
Tibisay Vielma <vielmatorotibisay@gmail.com>
2021-03-17 11:03:17 +0100

    flask_cookie created

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/app.py
@@ -0,0 +1,76 @@
+from flask import Flask
+from {{cookiecutter.app_name}} import api
+from {{cookiecutter.app_name}} import auth
+from {{cookiecutter.app_name}}.extensions import apispec
+from {{cookiecutter.app_name}}.extensions import db
+from {{cookiecutter.app_name}}.extensions import jwt
+from {{cookiecutter.app_name}}.extensions import migrate
+
+{%- if cookiecutter.use_celery == "yes"%}, celery{% endif%}
+
+
+def create_app(testing=False):
+    """Application factory, used to create application"""
+    app = Flask("{{cookiecutter.app_name}}")
+    app.config.from_object("{{cookiecutter.app_name}}.config")
+
+    if testing is True:
+        app.config["TESTING"] = True
+
+    configure_extensions(app)
+    configure_apispec(app)
+    register_blueprints(app)
+{%- if cookiecutter.use_celery == "yes" %}
+    init_celery(app)
+{%- endif %}
+
+    return app
+
+
+def configure_extensions(app):
+    """configure flask extensions"""
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+
+def configure_apispec(app):
+    """Configure APISpec for swagger support"""
+    apispec.init_app(app, security=[{"jwt": []}])
+    apispec.spec.components.security_scheme(
+        "jwt", {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
+    )
+    apispec.spec.components.schema(
+        "PaginatedResult",
+        {
+            "properties": {
+                "total": {"type": "integer"},
+                "pages": {"type": "integer"},
+                "next": {"type": "string"},
+                "prev": {"type": "string"},
+            }
+        },
+    )
+
+
+def register_blueprints(app):
+    """register all blueprints for application"""
+    app.register_blueprint(auth.views.blueprint)
+    app.register_blueprint(api.views.blueprint)
+{%- if cookiecutter.use_celery == "yes" %}
+
+
+def init_celery(app=None):
+    app = app or create_app()
+    celery.conf.update(app.config.get("CELERY", {}))
+
+    class ContextTask(celery.Task):
+        """Make celery tasks work with Flask app context"""
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+{%- endif %}
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
@@ -0,0 +1,26 @@
+"""Extensions registry
+
+All extensions here are used as singletons and
+initialized in application factory
+"""
+from flask_sqlalchemy import SQLAlchemy
+from passlib.context import CryptContext
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+{%- if cookiecutter.use_celery == "yes" %}
+from celery import Celery
+{%- endif %}
+
+from {{cookiecutter.app_name}}.commons.apispec import APISpecExt
+
+
+db = SQLAlchemy()
+jwt = JWTManager()
+ma = Marshmallow()
+migrate = Migrate()
+apispec = APISpecExt()
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+{%- if cookiecutter.use_celery == "yes" %}
+celery = Celery()
+{%- endif %}
